fe70a53d7d7e1fd7204fff89aa23c64b644b5aca
park hyeong gil <gudrlf7913@naver.com>
2021-06-15 02:00:20 +0900

    랜딩 페이지에 비디오 나타나게 하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import {Title, Row, Avatar} from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import moment from 'moment';
+import Axios from 'axios';
 
 function LandingPage() {
+
+    const [Video, setVideo] = useState([])
+
+    useEffect(() => {
+
+        Axios.get('/api/video/getVideos')
+            .then(res => {
+                if(res.data.success){
+                    setVideo(res.data.videos)
+                }else{
+                    alert('비디오 가져오기 실패')
+                }
+            })
+
+    }, [])
+
+    const renderCards = Video.map((video, index) => {
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration - (minutes * 60)));
+
+        return (
+            <Col key={index} lg={6} md={8} xs={24}>
+                    <a href={`/video/post/${video._id}`}>
+                        <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                        <div className="duration">
+                            <span>{minutes} : {seconds}</span>
+                        </div>
+                    </a>
+                    <br />
+                    <Meta 
+                        avatar={
+                            <Avatar src={video.writer.image} />
+                        }
+                        title={video.title}
+                        description=""
+                    />
+                    <span>{video.writer.name}</span>
+                    <span style={{ marginLeft: '3rem' }}>{video.views} views</span> - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+                </Col>
+        )
+    })
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <Title level={2}>Recommended</Title>
+            <hr />
+            <Row gutter={[32,16]}>
+                
+            </Row>
+        </div>
     )
 }
 
--- server/routes/video.js
+++ server/routes/video.js
@@ -47,7 +47,18 @@ router.post('/uploadVideo', (req, res) => {
     video.save((err, doc) => {
         if(err) return res.json({ success:  false, err });
         res.status(200).json({ success: true });
-    })
+    })  
+});
+
+router.get('/getVideos', (req, res) => {
+
+    // 비디오를 db에서 가져오기
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
 });
 
 router.post('/thumbnail', (req, res) => {
