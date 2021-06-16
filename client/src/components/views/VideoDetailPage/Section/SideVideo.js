f3c369c40115cf3e27ce15e1c4ae14c7b11d0c75
park hyeong gil <gudrlf7913@naver.com>
2021-06-16 12:14:41 +0900

    디테일 비디오 페이지에 Side 비디오 생성

---
+++ client/src/components/views/VideoDetailPage/Section/SideVideo.js
@@ -0,0 +1,53 @@
+import React, {useEffect} from 'react'
+import Axios from 'axios'
+
+function SideVideo() {
+
+    const [sideVideos, setSideVideos] = useState([])
+
+    useEffect(() => {
+        Axios.get('/api/video/getVideos')
+            .then(res => {
+                if(res.data.success){
+                    setSideVideos(res.data.videos)
+                }else{
+                    alert('Failed to get Videos')
+                }
+            })
+    }, [])
+    
+    const renderSideVideo = sideVideos.map((video, index) => {
+
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor(video.duration - (minutes * 60));
+
+        return <div key={index} style={{ display: 'flex', marginBottom: '1rem', padding: '0 2rem' }}>
+            <div style={{ width: '40%', marginRight: '1rem' }}>
+                <a href>
+                    <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                </a>
+            </div>
+
+            <div style={{ width: '50%', height: '100%' }}>
+                <a href style={{ color: 'gray' }}>
+                    <span style={{ fontSize: '1rem', color: 'black' }}>{video.title}</span>
+                    <span>{video.writer.name}</span><br />
+                    <span>{video.views} views </span><br />
+                    <span>{minutes} : {seconds}</span>
+                </a>
+            </div>
+        </div>
+
+    })
+
+    return (
+        <React.Fragment>
+            <div style={{ marginTop: '3rem' }} />
+
+            {renderSideVideo}
+
+        </React.Fragment>
+    )
+}
+
+export default SideVideo
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import {Col, Row, List, Avatar} from 'antd'
 import Axios from 'axios'
+import SideVideo from './Section/SideVideo'
 
 function VideoDetailPage(props) {
 
@@ -44,7 +45,7 @@ function VideoDetailPage(props) {
                 </Col>
     
                 <Col lg={6} xs={24}>
-                    Side Videos
+                    <SideVideo />
                 </Col>
             </Row>
         )
