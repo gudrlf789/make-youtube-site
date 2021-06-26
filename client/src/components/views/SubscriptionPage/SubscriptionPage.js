3aeaa5a4c42276bde0a8ff3f724d048ae36dcf35
park hyeong gil <gudrlf7913@naver.com>
2021-06-26 23:32:45 +0900

    구독 비디오 페이지 만들기

---
+++ client/src/components/views/SubscriptionPage/SubscriptionPage.js
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react'
+import { FaCode } from "react-icons/fa";
+import {Title, Row, Avatar} from 'antd';
+import Meta from 'antd/lib/card/Meta';
+import moment from 'moment';
+import Axios from 'axios';
+
+function SubscriptionPage() {
+    const [Video, setVideo] = useState([])
+
+    useEffect(() => {
+        
+        const subscriptionVariables = {
+            userFrom: localStorage.getItem('userId')
+        }
+
+        Axios.post('/api/video/getSubscriptionVideo', subscriptionVariables)
+            .then(res => {
+                if(res.data.success){
+                    setVideo(res.data.videos)
+                }else{
+                    alert('비디오 가져오기 실패')
+                }
+            })
+
+    }, [])
+
+    const renderCards = Video.map((video, index) => {
+        var minutes = Math.floor(video.duration / 60);
+        var seconds = Math.floor((video.duration - (minutes * 60)));
+
+        return (
+            <Col lg={6} md={8} xs={24}>
+                    <div style={{ position: 'relative' }}>
+                        <a href={`/video/${video._id}`}>
+                            <img style={{ width: '100%' }} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                            <div className="duration"
+                                style={{
+                                    bottom: 0, right: 0, position: 'absolute', margin: '4px',
+                                    color: '#fff', backgroundColor: 'rgba(17, 17, 17, 0.8)', opacity: 0.8,
+                                    padding: '2px 4px', borderRadius: '2px', letterSpacing: '0.5px', fontWeight: '500',
+                                    lineHeight: '12px' 
+                                }}
+                            >
+                                <span>{minutes} : {seconds}</span>
+                            </div>
+                        </a>
+                    </div>
+                    <br />
+                    <Meta 
+                        avatar={
+                            <Avatar src={video.writer.image} />
+                        }
+                        title={video.title}
+                        description=""
+                    />
+                    <span>{video.writer.name}</span>
+                    <span style={{ marginLeft: '3rem' }}>{video.views} views</span> - <span>{moment(video.createdAt).format("MMM Do YY")}</span>
+                </Col>
+        )
+    })
+
+    return (
+        <div style={{ width: '85%', margin: '3rem auto' }}>
+            <Title level={2}>Recommended</Title>
+            <hr />
+            <Row gutter={[32,16]}>
+                
+            </Row>
+        </div>
+    )
+}
+
+export default SubscriptionPage
--- server/routes/video.js
+++ server/routes/video.js
@@ -4,6 +4,7 @@ const { Video } = require("../models/Video");
 const { auth } = require("../middleware/auth");
 const multer = require('multer');
 var ffmpeg = require("fluent-ffmpeg");
+const Subscriber = require('../models/Subscriber');
 
 // storage multer config
 let storage = multer.diskStorage({
@@ -68,6 +69,30 @@ router.get('/getVideos', (req, res) => {
         .exec((err, videos) => {
             if(err) return res.status(400).send(err);
             res.status(200).json({ success: true, videos })
+        })
+});
+
+router.post('/getSubscriptionVideos', (req, res) => {
+
+    // 자신의 아이디를 가지고 구독하는 사람들을 찾는다
+    Subscriber.find({ userFrom: req.body.userFrom })
+        .exec(( err, subscriberInfo ) => {
+            if(err) return res.status(400).send(err);
+
+            let subscribedUser = [];
+
+            subscriberInfo.map((subscriber, i) => {
+                subscribedUser.push(subscriber.userTo);
+            })
+
+            // 찾은 사람들의 비디오를 가지고 온다.
+
+            Video.find({ writer: { $in:  subscribedUse }})
+                .populate('writer')
+                .exec((err, videos) => {
+                    if(err) return res.status(400).send(err);
+                    res.status(200).json({ success: true, videos })
+                })
         })
 });
 
